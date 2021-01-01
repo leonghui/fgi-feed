621417cf6b4691a3dd485c8f8f3a00a8329999bc
Leong Hui Wong <wong.leonghui@gmail.com>
2021-01-01 23:25:29 +0800

    Rewrite using dataclasses

--- fgi_feed.py
+++ fgi_feed.py
@@ -1,16 +1,24 @@
-from cssutils import parseStyle
 from datetime import datetime
 from pytz import timezone
-
-import requests
+from requests import Session
 from bs4 import BeautifulSoup
 from flask import abort
+from dataclasses import asdict
+from enum import Enum, auto
 
-JSONFEED_VERSION_URL = 'https://jsonfeed.org/version/1.1'
+from json_feed_data import JsonFeedTopLevel, JsonFeedItem
+
 MONEY_CNN_URL = 'https://money.cnn.com'
 FGI_URI = '/data/fear-and-greed/'
 FGI_FAVICON_URI = '/favicon.ico'
 CNN_TZ = timezone('US/Eastern')
+
+session = Session()
+
+
+class ROUND(Enum):
+    DAY = auto()
+    HOUR = auto()
 
 
 def extract_datetime(text, default_tz):
@@ -34,51 +42,61 @@ def extract_datetime(text, default_tz):
     return datetime_obj.astimezone(timezone('UTC'))
 
 
+# modified from https://stackoverflow.com/a/24893252
+def remove_empty_from_dict(d):
+    if isinstance(d, dict):
+        return dict((k, remove_empty_from_dict(v)) for k, v in d.items() if v and remove_empty_from_dict(v))
+    elif isinstance(d, list):
+        return [remove_empty_from_dict(v) for v in d if v and remove_empty_from_dict(v)]
+    else:
+        return d
+
+
 def get_latest_fgi(logger):
-    page_response = requests.get(MONEY_CNN_URL + FGI_URI)
+    page_url = MONEY_CNN_URL + FGI_URI
+
+    logger.debug(f'Querying endpoint: {page_url}')
+    page_response = session.get(page_url)
 
     # return HTTP error code
     if not page_response.ok:
-        abort(500, f"HTTP status from source: {page_response.status_code}")
+        logger.debug('Error from source, dumping input:')
+        logger.debug(page_response.text)
+        abort(
+            500, description=f"HTTP status from source: {page_response.status_code}")
 
-    page_content = page_response.text
+    page_soup = BeautifulSoup(page_response.text, features='html.parser')
+    feed_title = page_soup.title.text if page_soup else None
 
-    page_soup = BeautifulSoup(page_content, features='html.parser')
-    feed_title = page_soup.title.text
-    feed_url = MONEY_CNN_URL + FGI_URI
+    json_feed = JsonFeedTopLevel(
+        items=[],
+        title=feed_title,
+        home_page_url=page_url,
+        favicon=MONEY_CNN_URL + FGI_FAVICON_URI
+    )
 
-    output = {
-        'version': JSONFEED_VERSION_URL,
-        'title': feed_title,
-        'home_page_url': feed_url,
-        'favicon': MONEY_CNN_URL + FGI_FAVICON_URI
-    }
-
-    chart_section = page_soup.find(id='needleChart')
-
-    items_list = []
+    chart_section = page_soup.select_one('div#needleChart')
 
     if chart_section:
         fgi_values = chart_section.select('ul > li')
         fgi_value = next(iter(fgi_values), None)
 
-        date_section = page_soup.find(id='needleAsOfDate')
+        date_section = page_soup.select_one('div#needleAsOfDate')
 
-        fgi_datetime = extract_datetime(date_section.text, CNN_TZ)
+        fgi_datetime = extract_datetime(date_section.text, CNN_TZ).isoformat(
+            'T') if date_section else None
 
-        item = {
-            'id': fgi_datetime.isoformat('T'),  # use timestamp as unique id
-            'url': feed_url,
-            'title': f"Fear & Greed Index - {date_section.text}",
-            'date_published': fgi_datetime.isoformat('T'),
-            'content_text': fgi_value.text
-        }
+        feed_item = JsonFeedItem(
+            id=fgi_datetime,  # use timestamp as unique id
+            url=page_url,
+            title=f"Fear & Greed Index - {date_section.text}",
+            date_published=fgi_datetime,
+            content_text=fgi_value.text
+        )
 
-        items_list.append(item)
+        json_feed.items.append(feed_item)
 
     else:
         logger.warning('Chart section not found')
 
-    output['items'] = items_list
-
-    return output
+    return remove_empty_from_dict(asdict(json_feed))
--- server.py
+++ server.py
@@ -1,19 +1,17 @@
-from flask import Flask, jsonify, abort
+from flask import Flask, jsonify
 from flask.logging import create_logger
+
 from fgi_feed import get_latest_fgi
 
 app = Flask(__name__)
 logger = create_logger(app)
 
+
 @app.route('/', methods=['GET'])
-def form():
-    try:
-        output = get_latest_fgi(logger)
-        response = jsonify(output)
-        response.mimetype = 'application/feed+json'
-        return response
-    except Exception:
-        abort(500, description='Error generating output')
+def process_query():
+    output = get_latest_fgi(logger)
+
+    return jsonify(output)
 
 
 if __name__ == '__main__':
