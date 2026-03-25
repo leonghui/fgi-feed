e3ad8a6a118f8f4b2f6a4d6af0615fdc6f9e5309
leonghui <leonghui@users.noreply.github.com>
2026-03-25 19:35:16 +0000

    Fix logging

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 from fastapi.applications import FastAPI
-import uvicorn
-
 from fgi_feed import ROUNDING, get_latest_fgi
 from json_feed_data import JsonFeedTopLevel
+import uvicorn
+from uvicorn.config import LOGGING_CONFIG
 
 app: FastAPI = FastAPI()
-# app.config.update({'JSONIFY_MIMETYPE': 'application/feed+json'})
+
+log_config = LOGGING_CONFIG
+log_config["formatters"]["access"]["fmt"] = (
+    '%(asctime)s - %(levelprefix)s %(client_addr)s - "%(request_line)s" %(status_code)s'
+)
+log_config["formatters"]["default"]["fmt"] = "%(asctime)s - %(levelprefix)s %(message)s"
 
 
 @app.get(path="/")
@@ -16,4 +21,4 @@ def root(method: ROUNDING | None = None):
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000, log_config=log_config)
